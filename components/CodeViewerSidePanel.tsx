274cda0c1a0d4675998c62259ac209f1a0a35fcc
nikayel <alinikayeljamal@gmail.com>
2025-11-15 11:47:03 -0800

    feat: add useEffect import for ESC key handler in workspace file viewer

--- components/CodeViewerSidePanel.tsx
+++ components/CodeViewerSidePanel.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react"
+import React, { useEffect } from "react"
 import dynamic from "next/dynamic"
 import { X } from "lucide-react"
 import { Button } from "@/components/ui/button"
