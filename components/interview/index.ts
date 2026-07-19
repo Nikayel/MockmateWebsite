474992693aa0c5786109a22b288e72beeef030b7
Nikayel <alinikayeljamal@gmail.com>
2026-07-19 08:47:01 -0700

    chore(interview): drop dead component exports from the interview barrel

--- components/interview/index.ts
+++ components/interview/index.ts
@@ -9,26 +9,16 @@
 export { ScenarioBrowser } from "./ScenarioBrowser"
 export { PatternBrowser } from "./PatternBrowser"
 export { DSARoadmap } from "./DSARoadmap"
-export { ProblemPanel } from "./ProblemPanel"
 export { InterviewerChat, AIChatPartner } from "./InterviewerChat"
-export { InterviewEditor } from "./InterviewEditor"
 
 // ScenarioBrowser sub-components
 export { ScenarioCard } from "./ScenarioCard"
 export { ScenarioFilters } from "./ScenarioFilters"
 export { ScenarioSearchBar } from "./ScenarioSearchBar"
 
-// Minimal AI companion for reduced distraction
-export { MinimalAICompanion } from "./MinimalAICompanion"
-
 // Utilities
 export { InterviewTimer } from "./InterviewTimer"
 export { TestResultsPanel } from "./TestResultsPanel"
-export { HintsPanel } from "./HintsPanel"
-
-// RAG-enhanced hints
-export { RAGHintCard } from "./RAGHintCard"
-export { EnhancedHintsPanel } from "./EnhancedHintsPanel"
 
 // Voice mode toggle for live transcription
 export { VoiceModeToggle } from "./VoiceModeToggle"
